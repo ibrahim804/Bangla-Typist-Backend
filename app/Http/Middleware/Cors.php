244bd681ebec6197b5be930e57e465b730567e36
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-04-09 19:40:39 +0600

    Cors file changed for testing

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,22 +19,23 @@ class Cors
      */
     public function handle($request, Closure $next)
     {                                                                                                                  
-        // $allowed = array('http://192.168.0.156:4200');  
+        $allowed = array('https://bangla-typist.herokuapp.com');  
 
-        // if(isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed)){
-        //     return $next($request)
-        //         ->header('Access-Control-Allow-Origin', $_SERVER['HTTP_ORIGIN'])
-        //         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
-        //         // ->header('Access-Control-Allow-Credentials', 'true')
-        //         // ->header('Access-Control-Max-Age', '10000')
-        //         ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, X-Requested-With');
-        // }
+        if(isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed)){
+            return $next($request)
+                ->header('Access-Control-Allow-Origin', $_SERVER['HTTP_ORIGIN']);
+                // ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
+                // // ->header('Access-Control-Allow-Credentials', 'true')
+                // // ->header('Access-Control-Max-Age', '10000')
+                // ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, X-Requested-With');
+        }
 
         // return response()->json($this->getErrorMessage('Request from an unexpected origin.'));
 
         // return $next($request);
 
-        return $next($request)->header('Access-Control-Allow-Origin', '*');
+        // It is final
+        // return $next($request)->header('Access-Control-Allow-Origin', '*');
 
         // return $next($request)
         //     ->header('Access-Control-Allow-Origin', '*')
